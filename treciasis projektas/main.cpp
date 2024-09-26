700b5edefbdc15e5351dcfacffc6edc2b4be700e
Agnė Gaižutytė <s057931@ad.viko.lt>
2024-09-26 11:37:47 +0300

    pridejau projekta trecia

--- treciasis projektas/main.cpp
+++ treciasis projektas/main.cpp
@@ -1,6 +1,89 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+//void getPlayerInfo() {
+  //  string name = "Donatas";
+  //  string surname = "Sabonis";
+  //  string team = "Sacramento Kings";
+
+ //   cout << "Zaidejas " << name << " " << surname << " " << " zaidzia " << team << endl;
+//}
+
+//int getRandomNumber();
+//double getAverage(double,double);
+
+// int main()
+
+//{
+   // getPlayerInfo();
+   // cout << "Atsitiktinis skaicius: " << getRandomNumber() << endl;
+    //cout << "Skaiciu vidurkis: " << getAverage(45, 125.8) << endl;
+   // return 0;
+//}
+
+//int getRandomNumber () {
+  //  int randomNum = rand() % 100;
+    //return randomNum;
+
+//}
+
+//double getAverage (double num1, double num2) {
+  //  return (num1 + num2)/2;
+//}
+
+//cout << "--------------------------------------------------------------1---------------------------------------" << endl;
+
+//void getStudentsInfo() {
+ //   string name = "Agne";
+  //  string decision = "Pasirinkau nes patinka kodai ir nera daug ekonomikos.";
+
+   // cout << "Vardas " << name << ". Kodel pasirinkau sita studiju programa: " << decision << endl;
+
+//}
+
+//int main() {
+
+  //  getStudentsInfo();
+  //  getStudentsInfo();
+
+
+   // return 0;
+//}
+
+// cout << "----------------------------------------------------------------------2------------------------------------------------" << endl;
+
+/*
+void getCollegeInfo() {
+    string firstw = "Vilniaus";
+    string secondw = "kolegija";
+
+    cout << firstw << " " << secondw << endl;
+}
+
+void getFacultyInfo() {
+    string first = "Elektronikos";
+    string second = "ir";
+    string third = "informatikos";
+    string fourth = "fakultetas";
+
+    cout << first << " " << second << " " << third << " " << fourth << endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    getCollegeInfo();
+    getFacultyInfo();
+
+    return 0;
+}
+*/
+
+// cout << "----------------------------------------------------------3---------------------------------------" << endl;
+
+int main () {
+
     return 0;
 }
