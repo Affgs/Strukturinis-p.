c779043d7704d95b9c20efe3c8942251b5fdf4bf
Agnė Gaižutytė <s057931@ad.viko.lt>
2024-09-26 10:26:10 +0300

    pridejau projekta trecia

--- Pirmasis projektas/main.cpp
+++ Pirmasis projektas/main.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+   //string vardas = "Agne";
+   // string pavarde = "Gaizutyte";
+   // string studprogpav = "Programu sistemos";
+   // string grupe;
+   // int amzius;
+    //int kursas;
+   // amzius = 19;
+   // kursas = 1;
+   // grupe = "PI24";
+
+    //cout << vardas << " " << pavarde << " " << amzius << " " << grupe << " " << kursas << " " << studprogpav << endl;
+
+//cout << "------------------------------------2--------------------------------------------"" << endl;
+
+    // string marke = "BMW";
+   // string modelis = "M4";
+   // string spalva = " raudonos";
+  //  int metai = 2014;
+  //  float litrazas = 6.7;
+
+   // cout << "Automobilis " << marke << " " << modelis << " yra pagamintas " << metai << " metais. Jo motoras " << litrazas << " litrazo. Automobilis yra " << spalva << " spalvos." endl;
+
+//cout << "--------------------------------------3----------------------------------------------" << endl;
+
+   // string klubov = "Kauno";
+   // string klubop = "zalgiris";
+   // int metai = 1998;
+   // string savininkasv = "Paulius";
+   // string savininkasp = "Jankunas";
+   // string arenav = "Zalgirio";
+   // string arenap = "arena";
+   // int vietusk = 15415;
+
+    //cout << "Klubas " << klubov << " " << klubop << ". Ikurtas " << metai << "." << endl;
+    //cout << "Savininkas yra " << savininkasv << " " << savininkasp << ". Arena " << arenav << " " << arenap << ",o vietu skaicius yra " << vietusk << "." << endl;
+
+   //cout << "-----------------------------------4---------------------------------------------------------------------" << endl;
+
+ //int a = 13;
+ //int b = 5;
+// float c = 17.5;
+
+// cout << a+b-c << endl;
+// cout << 15/2+c << endl;
+// cout << a/static_cast<double>(b)+2*c << endl;
+// cout << 14%3+6.3+b/a << endl;
+// cout << static_cast<int>(c)%5+a-b << endl;
+// cout << 13.5/2+4.0*3.5+18 << endl;
+
+ //cout << "-------------------------------------5-------------------------------------------------------------------------------" << endl;
+
+ //int m=7;
+ //int g=13;
+ //int l=52;
+ //float r=9.3;
+ //float p=15.7;
+
+ //cout << (m+g+l+r+p)/5 << endl;
+
+ //cout << "-----------------------------------------6-----------------------------------------------------" << endl;
+
+ int a;
+ int b;
+ int c=25;
+ a=c%2;
+ b=c-a;
+ cout << a << " " << b << endl;
+
+
+ return 0;
 }
